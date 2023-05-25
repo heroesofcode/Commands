55995f68b73ba295bae37406aa29ab6157b537e4
João Lucas <joaolucasfp2001@gmail.com>
2023-05-24 22:20:51 -0300

    Setup header

--- commands.sh
+++ commands.sh
@@ -10,6 +10,11 @@ echo "                                                                          
  ░░░░░░   ░░░░░░  ░░░░░ ░░░ ░░░░░ ░░░░░ ░░░ ░░░░░  ░░░░░░░░ ░░░░ ░░░░░  ░░░░░░░░ ░░░░░░  
                                                                                          
                                                                                          
-                                                                                         "
+ "
 
 echo "Welcome to commands"
+echo "Commands was written in Shell Script to make everyday linux commands easier"
+echo "João Lucas"
+echo "
+===============================================================================================
+"
